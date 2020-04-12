5958c08ce11cf26bf99e1de92748a604b9484142
BobHasNoSoul <antonyedwards93@gmail.com>
2020-04-12 11:25:16 +0100

    Delete cronfirework.sh

--- cronfirework.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo cp ./animation/stylefix.css /usr/lib/jellyfin/bin/jellyfin-web/components/htmlvideoplayer/style.css
-sudo cp ./animation/fireworks.html /usr/lib/jellyfin/bin/jellyfin-web/index.html
-sudo cp ./animation/fireworks.css /usr/lib/jellyfin/bin/jellyfin-web/fireworks.css
