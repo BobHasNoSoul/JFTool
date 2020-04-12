e2f1a1efc15f1ea0d7f00a67f9ae4feb2b54c157
BobHasNoSoul <antonyedwards93@gmail.com>
2020-04-12 11:25:55 +0100

    Delete cronhearts.sh

--- cronhearts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo cp ./animation/stylefix.css /usr/lib/jellyfin/bin/jellyfin-web/components/htmlvideoplayer/style.css
-sudo cp ./animation/valentines.html /usr/lib/jellyfin/bin/jellyfin-web/index.htm
