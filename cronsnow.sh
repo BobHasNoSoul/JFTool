eeae9caa9f5568b6662b6e00ee9b0d8cf68a6836
BobHasNoSoul <antonyedwards93@gmail.com>
2020-04-12 11:26:29 +0100

    Delete cronsnow.sh

--- cronsnow.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo cp ./animation/stylefix.css /usr/lib/jellyfin/bin/jellyfin-web/components/htmlvideoplayer/style.css
-sudo cp ./animation/snow.html /usr/lib/jellyfin/bin/jellyfin-web/index.html
